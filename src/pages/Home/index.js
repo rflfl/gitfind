412a0cd68e7421936da8af8b0962d103ae2ce778
Rafael Ferreira Lemes <40215566+rflfl@users.noreply.github.com>
2023-12-22 14:05:19 +0000

    feat: criação de componentes e montagem da Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,31 @@
+import { Header } from "../../components/Header";
+import background from "../../assets/background.png";
+
+import "./styles.css"
+
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <Header />
+      <div className="conteudo">
+        <img src={background} className="background" alt="background app" />
+        <div className="info">
+          <div>
+            <input name="usuario" placeholder="@username"/>
+            <button>Buscar</button>
+          </div>
+          <div className="perfil">
+            <img src="https://images.unsplash.com/photo-1457449940276-e8deed18bfff?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="profile" alt="profile" />
+            <div>
+              <h3>Nome do Usuario</h3>
+              <span>@Usuario</span>
+              <p>Descrição do perfil</p>
+            </div>
+          </div>
+          <hr />
+        </div>
+      </div>
+    </div>
   );
 }
 
